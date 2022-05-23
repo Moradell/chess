27c5cb0fd7be952065800b8e99bc4fc750ab2d62
Aleksey <themoradell@gmail.com>
2022-05-23 20:12:57 +0300

    KEY-23 add game options bar component

---
+++ src/components/GameOptionsBar/GameOptionsBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './GameOptionsBar.module.scss';
+
+export default function GameOptionsBar() {
+  return (
+    <div>
+        GameOptionsBar
+    </div>
+  );
+}
