2c6f279a6f8ac3b325e2a471962d16fa797065c7
Aleksey <themoradell@gmail.com>
2022-05-23 20:11:14 +0300

    KEY-22 add game information bar component

---
+++ src/components/GameInformationBar/GameInformationBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './GameInformationBar.module.scss';
+
+export default function GameInformationBar() {
+  return (
+    <div>
+        GameInformationBar
+    </div>
+  );
+}
