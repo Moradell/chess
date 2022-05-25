fac80df3a3dbe3332295f03ccac701c119be81a0
Aleksey <themoradell@gmail.com>
2022-05-25 09:57:38 +0300

    KEY-25 add game information slicer

---
+++ src/store/gameInformationSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface GameInformationState {
+
+}
+
+const initialState: GameInformationState = {
+    
+}
+
+
+const gameInformationSlice = createSlice({
+    name: 'gameInformation',
+    initialState: {
+
+    },
+    reducers: {
+
+    }
+});
